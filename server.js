dd1a48501ac6316f73b228a85818dad6a3ac2cbc
Wesley Parr <wxp9764@gmail.com>
2019-03-11 12:32:30 -0700

    all

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+const Pusher = require('pusher')
+
+// const pusher = new Pusher({
+//     appId: '338977',
+//     key: '3e6b0e8f2442b34330b7',
+//     secret: 'bafd22e1acf4f096c8f2',
+//     cluster: 'eu',
+//     encrypted: true
+// });
+
+var pusher = new Pusher({
+  appId: '733283',
+  key: 'ca7ab5c69d086e8b9bee',
+  secret: 'ec46a906793ed731deda',
+  cluster: 'us3',
+  encrypted: true
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname + '/app')))
+
+app.set('port', (process.env.PORT || 5000))
+
+let expensesList = {
+  data: [
+    {
+      date: 'April 15th 2017',
+      expense: 100,
+      income: 4000
+    },
+    {
+      date: 'April 22nd 2017',
+      expense: 500,
+      income: 2000
+    },
+    {
+      date: 'April 24th 2017',
+      expense: 1000,
+      income: 2300
+    },
+    {
+      date: 'April 29th 2017',
+      expense: 2000,
+      income: 1234
+    },
+    {
+      date: 'May 1st 2017',
+      expense: 500,
+      income: 4180
+    },
+    {
+      date: 'May 5th 2017',
+      expense: 4000,
+      income: 5000
+    }
+  ]
+}
+
+app.get('/finances', (req, res) => {
+  res.send(expensesList)
+})
+
+app.post('/expense/add', (req, res) => {
+  let expense = Number(req.body.expense)
+  let income = Number(req.body.income)
+  let date = req.body.date
+
+  let newExpense = {
+    date: date,
+    expense: expense,
+    income: income
+  }
+
+  expensesList.data.push(newExpense)
+
+  pusher.trigger('finance', 'new-expense', {
+    newExpense: expensesList
+  })
+
+  res.send({
+    success: true,
+    income: income,
+    expense: expense,
+    date: date,
+    data: expensesList
+  })
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
---
+++ src/components/Home.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="row">
+        <h2 class="title">Budget Tracker</h2>
+        <h3 class="subtitle">Expense and Income</h3>
+        <line-chart :chart-data="datacollection"></line-chart>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <form class="form" @submit.prevent="addExpenses">
+          <h4>Add New Entry</h4>
+          <div class="form-group">
+            <label>Expenses</label>
+            <input class="form-control" placeholder="How much did you spend?" type="number" v-model="expenseamount" required>
+          </div>
+          <div class="form-group">
+            <label>Income</label>
+            <input class="form-control" placeholder="How much did you earn?" type="number" v-model="incomeamount" required>
+          </div>
+          <div class="form-group">
+            <label>Date</label>
+            <input class="form-control" placeholder="Date" type="date" v-model="entrydate" required>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary">Add New Entry</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import moment from 'moment'
+  import Pusher from 'pusher-js'
+  import LineChart from '@/components/LineChart'
+
+  const socket = new Pusher('ca7ab5c69d086e8b9bee', {
+    cluster: 'us3',
+    encrypted: true
+  })
+
+  // const socket = new Pusher('APP_KEY', {
+  //   cluster: 'YOUR CLUSTER',
+  //   encrypted: true
+  // })
+
+//   var pusher = new Pusher({
+//   appId: '733283',
+//   key: 'ca7ab5c69d086e8b9bee',
+//   secret: 'ec46a906793ed731deda',
+//   cluster: 'us3',
+//   encrypted: true
+// });
+  const channel = socket.subscribe('finance')
+
+  export default {
+    name: 'home',
+    components: {LineChart},
+    data () {
+      return {
+        expense: null,
+        income: null,
+        date: null,
+        expenseamount: null,
+        incomeamount: null,
+        datacollection: null,
+        entrydate: null
+      }
+    },
+    created () {
+      this.fetchData()
+      this.fillData()
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        axios.get('/finances')
+          .then(response => {
+            console.log(response.data.data)
+            let results = response.data.data
+            let dateresult = results.map(a => a.date)
+            let expenseresult = results.map(a => a.expense)
+            let incomeresult = results.map(a => a.income)
+
+            this.expense = expenseresult
+            this.income = incomeresult
+            this.date = dateresult
+
+            this.datacollection = {
+              labels: this.date,
+              datasets: [
+                {
+                  label: 'Expense',
+                  backgroundColor: '#f87979',
+                  data: this.expense
+                },
+                {
+                  label: 'Income',
+                  backgroundColor: '#5bf8bf',
+                  data: this.income
+                }
+              ]
+            }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+      addExpenses () {
+        let expense = this.expenseamount
+        let income = this.incomeamount
+        let today = moment(this.entrydate).format('MMMM Do YYYY')
+
+        axios.post('/expense/add', {
+          expense: expense,
+          income: income,
+          date: today
+        })
+          .then(response => {
+            this.expenseamount = ''
+            this.incomeamount = ''
+            this.entrydate = ''
+            channel.bind('new-expense', function (data) {
+              let results = data.newExpense.data
+
+              let dateresult = results.map(a => a.date)
+              let expenseresult = results.map(a => a.expense)
+              let incomeresult = results.map(a => a.income)
+
+              this.expense = expenseresult
+              this.income = incomeresult
+              this.date = dateresult
+
+              this.datacollection = {
+                labels: this.date,
+                datasets: [
+                  {
+                    label: 'Expense',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: '#f87979',
+                    data: this.expense
+                  },
+                  {
+                    label: 'Income',
+                    backgroundColor: 'transparent',
+                    pointBorderColor: '#5bf8bf',
+                    data: this.income
+                  }
+                ]
+              }
+            })
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+
+      fetchData () {
+        channel.bind('new-expense', data => {
+          let _results = data.newExpense.data
+          let dateresult = _results.map(a => a.date)
+          let expenseresult = _results.map(a => a.expense)
+          let incomeresult = _results.map(a => a.income)
+          this.expense = expenseresult
+          this.income = incomeresult
+          this.date = dateresult
+          this.datacollection = {
+            labels: this.date,
+            datasets: [
+              {
+                label: 'Expense Charts',
+                backgroundColor: '#f87979',
+                data: this.expense
+              },
+              {
+                label: 'Income Charts',
+                backgroundColor: '#5bf8bf',
+                data: this.income
+              }
+            ]
+          }
+        })
+      }
+    }
+  }
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .title {
+    text-align: center;
+    margin-top: 40px;
+  }
+  .subtitle {
+    text-align: center;
+  }
+  .form {
+    max-width: 600px;
+    width: 100%;
+    margin: 20px auto 0 auto;
+  }
+  .form h4 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
